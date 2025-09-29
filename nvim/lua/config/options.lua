b2983ff81cd570beca51ea643593ed926158e931
Francisco Fernandez <fj.fernandez@live.com>
2025-09-29 10:32:34 -0400

    Added alacritty, nvim and wallpapers

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local keymap = vim.keymap
+
+keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "Mofiqul/dracula.nvim",
+    priority = 1000,
+    opts = { transparent_bg = true },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
