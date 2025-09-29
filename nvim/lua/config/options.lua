7af2f4ec026648dd35814133cf26df66cf9f8fd4
Francisco Fernandez <fj.fernandez@live.com>
2025-09-29 15:30:23 -0400

    Added vim-tmux-navigator

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = false
+
 local keymap = vim.keymap
 
 keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
